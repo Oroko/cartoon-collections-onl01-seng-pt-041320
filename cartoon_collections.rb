a1350a085f05378c717916349f1a86dbae7b254c
Abel Gechure <aoroko09@gmail.com>
2020-04-23 01:25:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  count = 0
+  array.each_with_index do |name, index|
+    
+  end
+  
 end
 
 def summon_captain_planet# code an argument here
