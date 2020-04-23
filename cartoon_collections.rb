151c25db979dfe06d591dcb2b2abd42c53c1f90c
Abel Gechure <aoroko09@gmail.com>
2020-04-23 01:51:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(array)# code an argument here
   i = 0 
   while i < array.length
     
-    if yield(array[i].length > 4)
+    if yield(array[i])
       return array[i]
     end
     i += 1
