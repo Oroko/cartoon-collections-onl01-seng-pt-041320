708e2b09879e1d5281b183bbf6793ef89a3cb143
Abel Gechure <aoroko09@gmail.com>
2020-04-23 01:45:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,14 @@ def summon_captain_planet(array)# code an argument here
     
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  i = 0 
+  while i < array.length
+    if 
+    yield(array[i])
+    i += 1
+  end
 end
 
 def find_the_cheese# code an argument here
